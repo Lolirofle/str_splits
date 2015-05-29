4a5a1fd4698ca9289bfc8deae63c5169ad625fc5
Lolirofle <lolipopple@hotmail.com>
2015-05-29 18:53:29 +0200

    Rewrite to iterators

--- src/lib.rs
+++ src/lib.rs
@@ -1,88 +1,106 @@
-#![feature(str_char)]
+#![feature(core,str_char)]
 
-//TODO: Rewrite to iterators
+extern crate core;
 
-pub fn splits<'l,T,F: FnMut(&'l str,&'l str) -> Option<T>>(s: &'l str,mut search: F) -> Option<T>{
-	let mut begin = "";
-	let mut end   = s;
+use core::iter::Iterator;
 
-	loop{
-		if end.is_empty(){
-			return None
-		}
+pub struct Splits<'l>{
+	str  : &'l str,
+	begin: &'l str,
+	end  : &'l str
+}
 
-		let head_len = end.char_at(0).len_utf8();
-		begin = unsafe{s.slice_unchecked(0,begin.len() + head_len)};
-		end = unsafe{end.slice_unchecked(head_len,end.len())};
-
-		if let Some(data) = search(begin,end){
-			return Some(data);
+impl<'l> Splits<'l>{
+	pub fn from_str(str: &'l str) -> Self{
+		Splits{
+			str  : str,
+			begin: "",
+			end  : str
 		}
 	}
 }
 
-pub fn splits_char<'l,T,F: FnMut(char,&'l str,&'l str) -> Option<T>>(s: &'l str,mut search: F) -> Option<T>{
-	let mut begin = "";
-	let mut end   = s;
+impl<'l> Iterator for Splits<'l>{
+	type Item = (&'l str,&'l str);
 
-	loop{
-		if end.is_empty(){
-			return None
+	fn next(&mut self) -> Option<<Self as Iterator>::Item>{
+		if self.end.is_empty(){
+			None
+		}else{
+			let head_len = self.end.char_at(0).len_utf8();
+			self.begin = unsafe{self.str.slice_unchecked(0,self.begin.len() + head_len)};
+			self.end = unsafe{self.end.slice_unchecked(head_len,self.end.len())};
+
+			Some((self.begin,self.end))
 		}
+	}
+}
 
-		let c = end.char_at(0);
-		let c_len = c.len_utf8();
 
-		end   = unsafe{end.slice_unchecked(c_len,end.len())};
 
-		if let Some(data) = search(c,begin,end){
-			return Some(data);
+pub struct SplitsChar<'l>{
+	str  : &'l str,
+	begin: &'l str,
+	end  : &'l str
+}
+
+impl<'l> SplitsChar<'l>{
+	pub fn from_str(str: &'l str) -> Self{
+		SplitsChar{
+			str  : str,
+			begin: "",
+			end  :  str
 		}
+	}
+}
 
-		begin = unsafe{s.slice_unchecked(0,begin.len() + c_len)};
+impl<'l> Iterator for SplitsChar<'l>{
+	type Item = (&'l str,char,&'l str);
+
+	fn next(&mut self) -> Option<<Self as Iterator>::Item>{
+		if self.end.is_empty(){
+			None
+		}else{
+			let c = self.end.char_at(0);
+			let c_len = c.len_utf8();
+
+			self.end = unsafe{self.end.slice_unchecked(c_len,self.end.len())};
+			let begin = self.begin;
+			self.begin = unsafe{self.str.slice_unchecked(0,self.begin.len() + c_len)};
+
+			Some((begin,c,self.end))
+		}
 	}
 }
 
 #[test]
 fn test_splits(){
-	let mut i = 0;
-	assert!(splits("0123456789",|begin,end|{
-		match i{
-			0 => {assert_eq!(begin,"0");assert_eq!(end,"123456789");},
-			1 => {assert_eq!(begin,"01");assert_eq!(end,"23456789");},
-			2 => {assert_eq!(begin,"012");assert_eq!(end,"3456789");},
-			3 => {assert_eq!(begin,"0123");assert_eq!(end,"456789");},
-			4 => {assert_eq!(begin,"01234");assert_eq!(end,"56789");},
-			5 => {assert_eq!(begin,"012345");assert_eq!(end,"6789");},
-			6 => {assert_eq!(begin,"0123456");assert_eq!(end,"789");},
-			7 => {assert_eq!(begin,"01234567");assert_eq!(end,"89");},
-			8 => {assert_eq!(begin,"012345678");assert_eq!(end,"9");},
-			9 => {assert_eq!(begin,"0123456789");assert_eq!(end,"");},
-			_ => unreachable!()
-		}
-		i+=1;
-		Some(())
-	}).is_some());
+	let mut splits = Splits::from_str("0123456789");
+	/*01*/assert_eq!(splits.next().unwrap(),("0","123456789"));
+	/*02*/assert_eq!(splits.next().unwrap(),("01","23456789"));
+	/*03*/assert_eq!(splits.next().unwrap(),("012","3456789"));
+	/*04*/assert_eq!(splits.next().unwrap(),("0123","456789"));
+	/*05*/assert_eq!(splits.next().unwrap(),("01234","56789"));
+	/*06*/assert_eq!(splits.next().unwrap(),("012345","6789"));
+	/*07*/assert_eq!(splits.next().unwrap(),("0123456","789"));
+	/*08*/assert_eq!(splits.next().unwrap(),("01234567","89"));
+	/*09*/assert_eq!(splits.next().unwrap(),("012345678","9"));
+	/*10*/assert_eq!(splits.next().unwrap(),("0123456789",""));
+	/*11*/assert!(splits.next().is_none());
 }
 
 #[test]
 fn test_splits_char(){
-	let mut i = 0;
-	assert!(splits_char("0123456789",|c,begin,end|{
-		match i{
-			0 => {assert_eq!(begin,"");assert_eq!(c,'0');assert_eq!(end,"123456789");},
-			1 => {assert_eq!(begin,"0");assert_eq!(c,'1');assert_eq!(end,"23456789");},
-			2 => {assert_eq!(begin,"01");assert_eq!(c,'2');assert_eq!(end,"3456789");},
-			3 => {assert_eq!(begin,"012");assert_eq!(c,'3');assert_eq!(end,"456789");},
-			4 => {assert_eq!(begin,"0123");assert_eq!(c,'4');assert_eq!(end,"56789");},
-			5 => {assert_eq!(begin,"01234");assert_eq!(c,'5');assert_eq!(end,"6789");},
-			6 => {assert_eq!(begin,"012345");assert_eq!(c,'6');assert_eq!(end,"789");},
-			7 => {assert_eq!(begin,"0123456");assert_eq!(c,'7');assert_eq!(end,"89");},
-			8 => {assert_eq!(begin,"01234567");assert_eq!(c,'8');assert_eq!(end,"9");},
-			9 => {assert_eq!(begin,"012345678");assert_eq!(c,'9');assert_eq!(end,"");},
-			_ => unreachable!()
-		}
-		i+=1;
-		Some(())
-	}).is_some());
+	let mut splits = SplitsChar::from_str("0123456789");
+	/*01*/assert_eq!(splits.next().unwrap(),("",'0',"123456789"));
+	/*02*/assert_eq!(splits.next().unwrap(),("0",'1',"23456789"));
+	/*03*/assert_eq!(splits.next().unwrap(),("01",'2',"3456789"));
+	/*04*/assert_eq!(splits.next().unwrap(),("012",'3',"456789"));
+	/*05*/assert_eq!(splits.next().unwrap(),("0123",'4',"56789"));
+	/*06*/assert_eq!(splits.next().unwrap(),("01234",'5',"6789"));
+	/*07*/assert_eq!(splits.next().unwrap(),("012345",'6',"789"));
+	/*08*/assert_eq!(splits.next().unwrap(),("0123456",'7',"89"));
+	/*09*/assert_eq!(splits.next().unwrap(),("01234567",'8',"9"));
+	/*10*/assert_eq!(splits.next().unwrap(),("012345678",'9',""));
+	/*11*/assert!(splits.next().is_none());
 }

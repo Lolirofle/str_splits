fcd94eab2543933edc694a2a06444c3bd1466720
Lolirofle <lolipopple@hotmail.com>
2016-12-12 02:12:18 +0100

    Some extra impls

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,81 @@
 extern crate core;
 
-use core::iter::Iterator;
-use core::slice;
+use core::iter::{DoubleEndedIterator,Iterator};
+use core::{slice,str};
 
-//TODO: RSplits or specialize?
-
+#[derive(Copy,Clone,Debug,Eq,PartialEq)]
 pub struct Splits<Seq>{
 	begin: Seq,
 	end  : Seq
 }
 
 impl<'l> Splits<&'l str>{
+	#[inline]
 	pub fn from_str(str: &'l str) -> Self{
 		Splits{
 			begin: unsafe{str.slice_unchecked(0,0)},
 			end  : str
 		}
+	}
+
+	#[inline]
+	pub fn as_str(self) -> &'l str{
+		unsafe{self.begin.slice_unchecked(0,self.begin.len()+self.end.len())}
+	}
+
+	/*
+	pub fn prev_forward(&mut self) -> Option<<Self as Iterator>::Item>{
+		self.begin.chars().next().map(|last|{
+			let last_len = last.len_utf8();
+
+			self.begin = unsafe{self.begin.slice_unchecked(last_len,self.begin.len())};
+
+			(self.begin,self.end)
+		})
+	}
+
+	pub fn next_back(&mut self) -> Option<<Self as Iterator>::Item>{
+		self.end.chars().next_back().map(|last|{
+			let last_len = last.len_utf8();
+
+			self.end = unsafe{self.end.slice_unchecked(0,self.end.len()-last_len)};
+
+			(self.begin,self.end)
+		})
+	}
+	*/
+
+	pub fn prev(&mut self) -> Option<<Self as Iterator>::Item>{
+		self.begin.chars().next_back().map(|last|{
+			let last_len = last.len_utf8();
+
+			self.begin = unsafe{self.begin.slice_unchecked(0,self.begin.len()-last_len)};
+			self.end   = unsafe{str::from_utf8_unchecked(slice::from_raw_parts(self.end.as_ptr().offset(-(last_len as isize)),self.end.len()+last_len))};
+
+			(self.begin,self.end)
+		})
+	}
+
+	pub fn first(&mut self) -> Option<<Self as Iterator>::Item>{
+		if self.begin==self.end{
+			None
+		}else{
+			self.begin = unsafe{self.begin.slice_unchecked(0,0)};
+			self.end   = unsafe{self.begin.slice_unchecked(0,self.begin.len()+self.end.len())};
+
+			Some((self.begin,self.end))
+		}
+	}
+
+	pub fn last(&mut self) -> Option<<Self as Iterator>::Item>{
+		self.end.chars().next_back().map(|_|{
+			let len = self.end.len();
+
+			self.begin = unsafe{self.begin.slice_unchecked(0,self.begin.len() + len)};
+			self.end   = unsafe{self.end.slice_unchecked(len,len)};
+
+			(self.begin,self.end)
+		})
 	}
 }
 
@@ -32,14 +92,38 @@ impl<'l> Iterator for Splits<&'l str>{
 			(self.begin,self.end)
 		})
 	}
+
+	#[inline]
+	fn size_hint(&self) -> (usize,Option<usize>){
+		let len = self.end.len();
+		(len,Some(len))
+	}
+
+	#[inline(always)]
+	fn last(mut self) -> Option<<Self as Iterator>::Item>{
+		(&mut self).last()
+	}
+}
+
+impl<'l> ExactSizeIterator for Splits<&'l str>{
+	#[inline(always)]
+	fn len(&self) -> usize{
+		self.end.len()
+	}
 }
 
 impl<'l,T> Splits<&'l [T]>{
+	#[inline]
 	pub fn from_slice(slice: &'l [T]) -> Self{
 		Splits{
 			begin: unsafe{slice::from_raw_parts(slice.as_ptr() , 0)},
 			end  : slice
 		}
+	}
+
+	#[inline]
+	pub fn as_slice(self) -> &'l [T]{
+		unsafe{slice::from_raw_parts(self.begin.as_ptr() , self.begin.len()+self.end.len())}
 	}
 }
 
@@ -54,16 +138,31 @@ impl<'l,T> Iterator for Splits<&'l [T]>{
 			(self.begin,self.end)
 		})
 	}
+
+	#[inline]
+	fn size_hint(&self) -> (usize,Option<usize>){
+		let len = self.end.len();
+		(len,Some(len))
+	}
+}
+
+impl<'l,T> ExactSizeIterator for Splits<&'l [T]>{
+	#[inline(always)]
+	fn len(&self) -> usize{
+		self.end.len()
+	}
 }
 
 
 
+#[derive(Copy,Clone,Debug,Eq,PartialEq)]
 pub struct FocusedSplits<Seq>{
 	begin: Seq,
 	end  : Seq
 }
 
 impl<'l> FocusedSplits<&'l str>{
+	#[inline]
 	pub fn from_str(str: &'l str) -> Self{
 		FocusedSplits{
 			begin: unsafe{str.slice_unchecked(0,0)},
@@ -73,6 +172,7 @@ impl<'l> FocusedSplits<&'l str>{
 }
 
 impl<'l,T> FocusedSplits<&'l [T]>{
+	#[inline]
 	pub fn from_slice(slice: &'l [T]) -> Self{
 		FocusedSplits{
 			begin: unsafe{slice::from_raw_parts(slice.as_ptr() , 0)},
@@ -130,11 +230,86 @@ mod test{
 		/*09*/assert_eq!(splits.next(),Some(("012345678","9")));
 		/*10*/assert_eq!(splits.next(),Some(("0123456789","")));
 		/*11*/assert_eq!(splits.next(),None);
+		/*12*/assert_eq!(splits.next(),None);
+	}
+
+	/*#[test]
+	fn Splits_str_back(){
+		let mut splits = Splits::from_str("0123456789");
+		/*01<*/assert_eq!(splits.next_back(),Some(("","012345678")));
+		/*01>*/assert_eq!(splits.next()     ,Some(("0","12345678")));
+		/*02<*/assert_eq!(splits.next_back(),Some(("0","1234567")));
+		/*02>*/assert_eq!(splits.next()     ,Some(("01","234567")));
+		/*03<*/assert_eq!(splits.next_back(),Some(("01","23456")));
+		/*03>*/assert_eq!(splits.next()     ,Some(("012","3456")));
+		/*04<*/assert_eq!(splits.next_back(),Some(("012","345")));
+		/*04>*/assert_eq!(splits.next()     ,Some(("0123","45")));
+		/*05<*/assert_eq!(splits.next_back(),Some(("0123","4")));
+		/*05>*/assert_eq!(splits.next()     ,Some(("01234","")));
+		/*06<*/assert_eq!(splits.next_back(),None);
+		/*06>*/assert_eq!(splits.next()     ,None);
+		/*07<*/assert_eq!(splits.next_back(),None);
+		/*07>*/assert_eq!(splits.next()     ,None);
+	}
+
+	#[test]
+	fn Splits_str_prev_forward(){
+		let mut splits = Splits::from_str("0123456789");
+		/*00 */assert_eq!((&mut splits).last() ,Some(("0123456789","")));
+		/*01<*/assert_eq!(splits.prev_forward(),Some(("123456789","")));
+		/*01>*/assert_eq!(splits.prev()        ,Some(("12345678","9")));
+		/*02<*/assert_eq!(splits.prev_forward(),Some(("2345678","9")));
+		/*02>*/assert_eq!(splits.prev()        ,Some(("234567","89")));
+		/*03<*/assert_eq!(splits.prev_forward(),Some(("34567","89")));
+		/*03>*/assert_eq!(splits.prev()        ,Some(("3456","789")));
+		/*04<*/assert_eq!(splits.prev_forward(),Some(("456","789")));
+		/*04>*/assert_eq!(splits.prev()        ,Some(("45","6789")));
+		/*05<*/assert_eq!(splits.prev_forward(),Some(("5","6789")));
+		/*05>*/assert_eq!(splits.prev()        ,Some(("","56789")));
+		/*06<*/assert_eq!(splits.prev_forward(),None);
+		/*06>*/assert_eq!(splits.prev()        ,None);
+		/*07<*/assert_eq!(splits.prev_forward(),None);
+		/*07>*/assert_eq!(splits.prev()        ,None);
+	}*/
+
+	#[test]
+	fn Splits_str_last(){
+		assert_eq!(Some(("0123456789","")),Splits::from_str("0123456789").last());
+		assert_eq!(Some(("012345678","")),Splits::from_str("012345678").last());
+		assert_eq!(Some(("01234567","")),Splits::from_str("01234567").last());
+		assert_eq!(Some(("0123456","")),Splits::from_str("0123456").last());
+		assert_eq!(Some(("012345","")),Splits::from_str("012345").last());
+		assert_eq!(Some(("01234","")),Splits::from_str("01234").last());
+		assert_eq!(Some(("0123","")),Splits::from_str("0123").last());
+		assert_eq!(Some(("012","")),Splits::from_str("012").last());
+		assert_eq!(Some(("01","")),Splits::from_str("01").last());
+		assert_eq!(Some(("0","")),Splits::from_str("0").last());
+		assert_eq!(None,Splits::from_str("").last());
+	}
+
+	#[test]
+	fn Splits_str_len(){
+		let mut iter = Splits::from_str("0123");
+		assert_eq!(4,iter.len());iter.next();
+		assert_eq!(3,iter.len());iter.next();
+		assert_eq!(2,iter.len());iter.next();
+		assert_eq!(1,iter.len());iter.next();
+		assert_eq!(0,iter.len());iter.next();
+		assert_eq!(0,iter.len());iter.next();
+
+		/*let mut iter = Splits::from_str("0123");
+		assert_eq!(4,iter.len());iter.next_back();
+		assert_eq!(3,iter.len());iter.next_back();
+		assert_eq!(2,iter.len());iter.next_back();
+		assert_eq!(1,iter.len());iter.next_back();
+		assert_eq!(0,iter.len());iter.next_back();
+		assert_eq!(0,iter.len());iter.next_back();*/
 	}
 
 	#[test]
 	fn Splits_str_empty(){
 		let mut splits = Splits::from_str("");
+		assert!(splits.next().is_none());
 		assert!(splits.next().is_none());
 	}
 
@@ -144,6 +319,25 @@ mod test{
 		let mut splits = Splits::from_str("a");
 		/*01*/assert_eq!(splits.next().unwrap(),("a",""));
 		/*02*/assert!(splits.next().is_none());
+		/*03*/assert!(splits.next().is_none());
+	}
+
+	#[test]
+	fn Splits_str_original(){
+		let mut splits = Splits::from_str("0123456789");
+		/*00*/               assert_eq!(splits.clone().as_str(),"0123456789");
+		/*01*/splits.next(); assert_eq!(splits.clone().as_str(),"0123456789");
+		/*02*/splits.next(); assert_eq!(splits.clone().as_str(),"0123456789");
+		/*03*/splits.next(); assert_eq!(splits.clone().as_str(),"0123456789");
+		/*04*/splits.next(); assert_eq!(splits.clone().as_str(),"0123456789");
+		/*05*/splits.next(); assert_eq!(splits.clone().as_str(),"0123456789");
+		/*06*/splits.next(); assert_eq!(splits.clone().as_str(),"0123456789");
+		/*07*/splits.next(); assert_eq!(splits.clone().as_str(),"0123456789");
+		/*08*/splits.next(); assert_eq!(splits.clone().as_str(),"0123456789");
+		/*09*/splits.next(); assert_eq!(splits.clone().as_str(),"0123456789");
+		/*10*/splits.next(); assert_eq!(splits.clone().as_str(),"0123456789");
+		/*11*/splits.next(); assert_eq!(splits.clone().as_str(),"0123456789");
+		/*12*/splits.next(); assert_eq!(splits.clone().as_str(),"0123456789");
 	}
 
 	#[test]
@@ -161,11 +355,13 @@ mod test{
 		/*09*/assert_eq!(splits.next(),Some((&[0u8,1,2,3,4,5,6,7,8] as &[u8],&[9u8] as &[u8])));
 		/*10*/assert_eq!(splits.next(),Some((&[0u8,1,2,3,4,5,6,7,8,9] as &[u8],&[] as &[u8])));
 		/*11*/assert_eq!(splits.next(),None);
+		/*12*/assert_eq!(splits.next(),None);
 	}
 
 	#[test]
 	fn Splits_slice_empty(){
 		let mut splits = Splits::from_slice(&[] as &[u8]);
+		assert!(splits.next().is_none());
 		assert!(splits.next().is_none());
 	}
 
@@ -176,6 +372,7 @@ mod test{
 		let mut splits = Splits::from_slice(&slice as &[u8]);
 		/*01*/assert_eq!(splits.next().unwrap(),(&[1u8] as &[u8],&[] as &[u8]));
 		/*02*/assert!(splits.next().is_none());
+		/*03*/assert!(splits.next().is_none());
 	}
 
 	#[test]
@@ -188,6 +385,7 @@ mod test{
 		/*04*/assert_eq!(splits.next(),Some((&[(),(),(),()] as &[()],&[()] as &[()])));
 		/*05*/assert_eq!(splits.next(),Some((&[(),(),(),(),()] as &[()],&[] as &[()])));
 		/*06*/assert_eq!(splits.next(),None);
+		/*07*/assert_eq!(splits.next(),None);
 	}
 
 	#[test]
@@ -204,11 +402,13 @@ mod test{
 		/*09*/assert_eq!(splits.next(),Some(("01234567",'8',"9")));
 		/*10*/assert_eq!(splits.next(),Some(("012345678",'9',"")));
 		/*11*/assert_eq!(splits.next(),None);
+		/*12*/assert_eq!(splits.next(),None);
 	}
 
 	#[test]
 	fn FocusedSplits_str_empty(){
 		let mut splits = FocusedSplits::from_str("");
+		assert_eq!(splits.next(),None);
 		assert_eq!(splits.next(),None);
 	}
 
@@ -217,6 +417,7 @@ mod test{
 		let mut splits = FocusedSplits::from_str("a");
 		/*01*/assert_eq!(splits.next(),Some(("",'a',"")));
 		/*02*/assert_eq!(splits.next(),None);
+		/*03*/assert_eq!(splits.next(),None);
 	}
 
 	#[test]
@@ -225,6 +426,7 @@ mod test{
 		/*01*/assert_eq!(splits.next(),Some(("",'a',"b")));
 		/*02*/assert_eq!(splits.next(),Some(("a",'b',"")));
 		/*03*/assert_eq!(splits.next(),None);
+		/*04*/assert_eq!(splits.next(),None);
 	}
 
 	#[test]
@@ -234,6 +436,7 @@ mod test{
 		/*02*/assert_eq!(splits.next(),Some(("a",'b',"c")));
 		/*03*/assert_eq!(splits.next(),Some(("ab",'c',"")));
 		/*04*/assert_eq!(splits.next(),None);
+		/*05*/assert_eq!(splits.next(),None);
 	}
 
 	#[test]
@@ -250,11 +453,13 @@ mod test{
 		/*09*/assert_eq!(splits.next(),Some(("01234567",'8',"9")));
 		/*10*/assert_eq!(splits.next(),Some(("012345678",'9',"")));
 		/*11*/assert_eq!(splits.next(),None);
+		/*12*/assert_eq!(splits.next(),None);
 	}
 
 	#[test]
 	fn FocusedSplits_slice_empty(){
 		let mut splits = FocusedSplits::from_slice(&[] as &[u8]);
+		assert_eq!(splits.next(),None);
 		assert_eq!(splits.next(),None);
 	}
 
@@ -264,6 +469,7 @@ mod test{
 		let mut splits = FocusedSplits::from_slice(&slice as &[u8]);
 		/*01*/assert_eq!(splits.next(),Some((&[] as &[_],&1,&[] as &[_])));
 		/*02*/assert_eq!(splits.next(),None);
+		/*03*/assert_eq!(splits.next(),None);
 	}
 
 	#[test]
@@ -273,6 +479,7 @@ mod test{
 		/*01*/assert_eq!(splits.next(),Some((&[] as &[_],&1,&[2u8] as &[_])));
 		/*02*/assert_eq!(splits.next(),Some((&[1u8] as &[_],&2,&[] as &[_])));
 		/*03*/assert_eq!(splits.next(),None);
+		/*04*/assert_eq!(splits.next(),None);
 	}
 
 	#[test]
@@ -283,5 +490,6 @@ mod test{
 		/*02*/assert_eq!(splits.next(),Some((&[1u8] as &[_],&2,&[3u8] as &[_])));
 		/*03*/assert_eq!(splits.next(),Some((&[1,2u8] as &[_],&3,&[] as &[_])));
 		/*04*/assert_eq!(splits.next(),None);
+		/*05*/assert_eq!(splits.next(),None);
 	}
 }
